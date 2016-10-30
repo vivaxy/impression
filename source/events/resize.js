2811be71bc7e45bcb35358efd3d0938a5c65617a
vivaxy <xyxuye2007@126.com>
2016-10-30 14:24:28 +0800

    resize

---
+++ source/events/resize.js
@@ -0,0 +1,29 @@
+/**
+ * @since 2016-10-30 14:24
+ * @author vivaxy
+ */
+
+export default (container) => {
+    let attached = false;
+
+    return (callback) => {
+        if (attached) {
+            return false;
+        } else {
+            attached = true;
+
+            // for calculating offset
+            if (container instanceof HTMLElement) {
+                const style = window.getComputedStyle(container);
+
+                if (style.position === 'static') {
+                    container.style.position = 'relative';
+                }
+            }
+
+            container.addEventListener('resize', callback);
+
+            return true;
+        }
+    };
+};
