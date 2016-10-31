099116da110b7f90b9908e86306e42d80d4af71f
vivaxy <xyxuye2007@126.com>
2016-10-31 13:37:18 +0800

    add observers.js

--- test/lib/presets.js
+++ test/lib/presets.js
@@ -2,3 +2,16 @@
  * @since 2016-10-31 13:33
  * @author vivaxy
  */
+
+import presets from '../../source/lib/presets';
+
+describe('presets', () => {
+    it('should change the container style', (done) => {
+        const container = document.querySelector('.container');
+        presets(container);
+        expect(container.style.position).to.equal('relative');
+        const element1 = container.querySelector('[data-impression-unique-id="1"]');
+        expect(element1.style.position).to.equal('static');
+        done();
+    });
+});
